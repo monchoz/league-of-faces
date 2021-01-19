d75e51c5d50a96170a50bebded1629f9c0c3f719
Ramon Zuniga <ramonzuniga9@gmail.com>
2021-01-18 23:24:04 -0600

    code cleanup, requirements, pipenv, readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 League of Legends A New Dawn cinematic champions recognition with AI.
 
 This project is not related with any work or colaboration with/from Riot Games. It's been created by myself with the only porpuse of exploring the capabilities of the use of AI on a video (besides I really like this game :p).
+
+We'll use Dlib's `get_frontal_face_detector`, along with the [68 point shape prediction](https://github.com/davisking/dlib-models) model (shape_predictor_68.dat).
+
+The script uses the face detection algorithm in Dlib to read any front faces. It uses the prediction points to create and crop the face then saves it as an image for each face detected.
+
+The images detected where used as training data to build a Deep Learning model that is used to predict the name of the characters (Champions) on the video.
--- detect.py
+++ detect.py
@@ -1,34 +1,31 @@
+import cv2
 import dlib
 from PIL import Image
 
-import cv2
-
-vs = cv2.VideoCapture('video.mp4')
+stream = cv2.VideoCapture('video.mp4')
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68.dat')
 
-frame = vs.read()
-fps = 60
+frame = stream.read()
 crop_width = 108
 incremental = 100
 simple_crop = True
 faces_dirname = 'images'
 
 while True:
-    _, frame = vs.read()
+    _, frame = stream.read()
 
+    # need grayscale for dlib face detection
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = []
 
     rects = detector(img_gray, 0)
     img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     print("Faces detected: %d" % (len(rects)))
 
+    # loop through detected faces
     for rect in rects:
-        face = {}
-
         shades_width = rect.right() - rect.left()
         shades_height = rect.bottom() - rect.top()
 
@@ -48,6 +45,7 @@ while True:
                 short_side = size_array[0]
                 crop_area = (rect.left() - size_array[0] , rect.top() - size_array[0], rect.right() + size_array[0], rect.bottom() + size_array[0])
 
+            # save cropped face to image locally
             cropped_image = image_to_crop.crop(crop_area)
             crop_size = (crop_width, crop_width)
             cropped_image.thumbnail(crop_size)
